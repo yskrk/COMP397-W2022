19d2f4170549edc52c691c29df8b1555d3ba8b04
Yusuke <ykuroki@my.centennialcollege.ca>
2022-02-04 21:19:02 -0500

    Added lab 4 project

---
+++ COMP397-W2022-Lab4/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // public values
+    [SerializeField] private float mouseSensitivity = 10.0f;
+    [SerializeField] private Transform playerBody;
+
+    // private values
+    private float XRotation = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        XRotation -= mouseY;
+        XRotation = Mathf.Clamp(XRotation, -90.0f, 90.0f);
+
+        // rotating left / right
+        transform.localRotation = Quaternion.Euler(XRotation, 0.0f, 0.0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
