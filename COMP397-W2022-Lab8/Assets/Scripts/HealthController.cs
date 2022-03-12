6a560214753be46915698c84b034ceead2c4e63a
Yusuke <ykuroki@my.centennialcollege.ca>
2022-03-12 16:45:54 -0500

    Added Lab 8

---
+++ COMP397-W2022-Lab8/Assets/Scripts/HealthController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    [SerializeField] private UIController controls;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            controls.TakeDamage(10);
+        }
+    }
+}
---
+++ COMP397-W2022-Lab8/Assets/Scripts/PlayerBehabiour.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehabiour : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    
+    [Header("Movement")]
+    [SerializeField] private float maxSpeed = 10.0f;
+    [SerializeField] private float gravity = -30.0f;
+    [SerializeField] private float jumpHeight = 3.0f;
+    [SerializeField] private Vector3 velocity;
+
+    [Header("Ground Detection")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundRadius = 0.5f;
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] bool isGrounded;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, groundMask);
+
+        if (isGrounded && velocity.y < 0.0f)
+        {
+            velocity.y = -2.0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // move
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * maxSpeed * Time.deltaTime);
+
+        // jump
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.gray;
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+    }
+}
